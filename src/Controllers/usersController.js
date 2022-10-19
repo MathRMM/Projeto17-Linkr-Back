18dee4c50d099da793398bd4bd82749a72859646
Math <math@pop-os.localdomain>
2022-10-18 23:28:01 -0300

    Iniciando rota usuario

--- src/Controllers/usersController.js
+++ src/Controllers/usersController.js
@@ -6,7 +6,17 @@ async function getUserByIdController(req, res){
 
     try {
         const user = await getUserById(id)
-        return res.status(200).send(user)
+        const objUser = {
+            userId: user[0].userId,
+            username: user[0].username,
+            userPicUrl: user[0].userPicUrl,
+            posts:user.map(e => {return {
+                postId: e.postId,
+                postLink: e.postLink,
+                postText: e.postText
+            }})
+        }
+        return res.status(200).send(objUser)
     } catch (error) {
         console.error(error)
         return res.sendStatus(500)
--- src/Repositories/usersRepositories.js
+++ src/Repositories/usersRepositories.js
@@ -2,14 +2,18 @@ import { connection } from "../DB/db.js";
 
 async function getUserById(id){
     return (await connection.query(`
-        SELECT 
-            id,
-            username,
-            email,
-            "picUrl"
-        FROM users
-        WHERE id = $1;
-    `, [id])).rows[0]
+    SELECT 
+        users.id AS "userId",
+        users.username,
+        users.email,
+        users."picUrl" AS "userPicUrl",
+        posts.id AS "postId",
+        posts.link AS "postLink",
+        posts."postText"
+    FROM users
+    JOIN posts ON posts."idUser" = users.id
+    WHERE posts."idUser" = $1;
+    `, [id])).rows
 }
 
 export {
