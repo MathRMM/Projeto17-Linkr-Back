92c3b028c1a217c43c38f60dc5ddaa30a26d2a1e
mateus <mateus.tigreewallet@gmail.com>
2022-10-20 17:57:22 -0300

    fazendo rota post de posts

---
+++ src/Server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import router from "./Routes/Router.js";
+
+dotenv.config();
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.get("/status", (req, res) => res.send("ok"));
+
+server.use(router);
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, console.log(`listen on port ${PORT}`));
---
+++ src/repository/postRepository.js
@@ -0,0 +1,28 @@
+import { connection } from "../DB/db";
+
+async function newPost(id, postText, link, metaDados) {
+  return connection.query(
+    `INSERT INTO posts ("idUser",postText,link,title,description,image)VALUES($1, $2, $3, $4, $5, $6)`,
+    [
+      id,
+      postText,
+      link,
+      metaDados.title,
+      metaDados.description,
+      metaDados.image,
+    ]
+  );
+}
+
+async function listPost() {
+  return connection.query(
+    `SELECT users.id, posts.id AS postId, users.username, users.picUrl AS profile, post.postText, posts.link, posts.title, posts.image , posts.description FROM
+     users JOIN posts ON users.id = posts."idUser"  ORDER BY postId DESC
+    LIMIT 20;`
+  );
+}
+
+export const postRepository = {
+  listPost,
+  newPost,
+};
