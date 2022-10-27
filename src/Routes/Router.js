0c4ee7c8be8e4e652136ca176a5a36bbb3cc03c7
Math <math@pop-os.localdomain>
2022-10-27 20:56:57 -0300

    resolvendo conflito no router

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -3,15 +3,10 @@ import express from "express";
 import loginRoutes from './loginRoutes.js'
 import usersRoutes from './usersRoutes.js'
 import likesRoutes from "./likesRoutes.js";
-<<<<<<< HEAD
 import updAndDel from './updateAndDeleteRotes.js';
 import postRouter from "./postRouter.js";
 import comments from "./CommentRoutes.js";
 import following from "./followingRoute.js";
-=======
-import updAndDel from './updateAndDeleteRotes.js'
-import postRouter from "./postRouter.js";
->>>>>>> main
 
 const router = express.Router();
 
@@ -19,12 +14,8 @@ router.use(loginRoutes);
 router.use(usersRoutes);
 router.use(likesRoutes);
 router.use(updAndDel);
-<<<<<<< HEAD
 router.use(postRouter);
 router.use(comments);
 router.use(following);
-=======
-router.use(postRouter)
->>>>>>> main
 
 export default router;
