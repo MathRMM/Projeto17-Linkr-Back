ae6908ca0a758afed65f2a66d0ab5d4779314559
Math <math@pop-os.localdomain>
2022-10-28 20:14:10 -0300

    resolvendo merge

--- src/DB/db.js
+++ src/DB/db.js
@@ -5,17 +5,17 @@ dotenv.config()
 
 const { Pool } = pg;
 
-/* const databaseConfig = {
-    connectionString: process.env.DATABASE_URL,
-} */
-
 const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+}
+
+/* const databaseConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DATABASE,
-}
+} */
 
 const connection = new Pool(databaseConfig);
 
