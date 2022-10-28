b281052abf523918b0f52f020ffb90b6d049d6c5
Cecília <ceciliagarcia@usp.br>
2022-10-28 16:42:47 -0300

    Centralizando as funções de follow

--- src/Repositories/followRepository.js
+++ src/Repositories/followRepository.js
@@ -71,3 +71,9 @@ export async function deleteFollowing(idUser, idFollowing) {
         ]
     );
 }
+
+export async function getFollowingsByidUser(id) {
+    return (
+        await connection.query(`SELECT * FROM following WHERE "idUser" = $1;`, [id])
+    )
+}
