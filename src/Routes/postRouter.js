a392ff5f4c8c9b69fd42ed2b70cada3356a52269
mateus <mateus.tigreewallet@gmail.com>
2022-10-28 13:58:06 -0300

    mesclando branch

--- src/Routes/postRouter.js
+++ src/Routes/postRouter.js
@@ -1,14 +1,18 @@
 import express from "express";
-import { createPost, getPosts, updatePostsController } from "../Controllers/postController.js";
+import {
+  creatPost,
+  getPosts,
+  updatePostsController,
+} from "../Controllers/postController.js";
 import { authenticateToken } from "../Middleware/authenticationMiddleware.js";
 import { validateSchema } from "../Middleware/postMiddleware.js";
 
 const postRouter = express.Router();
 
-postRouter.post("/timeline", validateSchema, createPost);
+postRouter.post("/timeline", validateSchema, creatPost);
 
 postRouter.get("/timeline", getPosts);
 
-postRouter.get('/count/posts', updatePostsController);
+postRouter.get("/count/posts", updatePostsController);
 
 export default postRouter;
